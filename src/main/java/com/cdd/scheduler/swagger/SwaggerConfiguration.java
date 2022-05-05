3e56b04fe282d0f34b9f5944dd914eaedcad53d2
Charles Dalisay <charles.dalisay@xurpas.com>
2022-05-05 14:05:08 +0800

    [4] Reconfigure swagger package path

--- src/main/java/com/cdd/scheduler/swagger/SwaggerConfiguration.java
+++ src/main/java/com/cdd/scheduler/swagger/SwaggerConfiguration.java
@@ -17,7 +17,7 @@ public class SwaggerConfiguration  {
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
-          .apis(RequestHandlerSelectors.basePackage("com.mbtc_empr_app.api"))              
+          .apis(RequestHandlerSelectors.basePackage("com.cdd.scheduler"))              
           .paths(PathSelectors.any())                          
           .build();                                           
     }
