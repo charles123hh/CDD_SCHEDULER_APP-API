c09b31497471d55c890fc99e8ca8a56bf5a581f7
Charles Dalisay <charles.dalisay@xurpas.com>
2022-05-10 09:29:59 +0800

    [7] Remove modeler dependency

--- src/main/java/com/cdd/scheduler/SchedulerApplication.java
+++ src/main/java/com/cdd/scheduler/SchedulerApplication.java
@@ -1,20 +1,12 @@
 package com.cdd.scheduler;
-
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SchedulerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchedulerApplication.class, args);
-	}
-
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
 	}
 
 }
